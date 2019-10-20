420e822c8f52370bc063c8ab9df417a9a3d211bf
Miquel Sabaté Solà <msabate@suse.com>
2019-10-20 20:44:43 +0200

    Added meta tags into the default layout
    This also includes a small Jekyll plugin that automatically guesses the OG image
    to be used depending on the availability of a thumbnail image.
    
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

---
+++ _plugins/twitter.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+# Copyright (C) 2019 Miquel Sabaté Solà <mikisabate@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+module Jekyll
+  # Render the OG image meta HTML tag for the currently rendered page.
+  class RenderOGImageTag < Liquid::Tag
+    # The path to be used when the current page does not have a specific image.
+    DEFAULT_PATH = "/images/default.png"
+
+    def render(context)
+      img  = normalize(context["raw_title"]) + ".png"
+      path = image_exists?(img) ? "/images/posts/#{img}" : DEFAULT_PATH
+      render_meta("og:image", "http://jo.mssola.com#{path}")
+    end
+
+    protected
+
+    # Returns true if the given image exists inside of /images/posts.
+    def image_exists?(image)
+      return false if image == ".png"
+
+      path = File.expand_path(File.join(File.dirname(__FILE__), "..", "images", "posts", image))
+      File.file?(path)
+    end
+
+    # Downcases the given string and replaces space characters with "-"
+    def normalize(str)
+      str.gsub(/\s/, "-").downcase
+    end
+
+    # Render an HTML meta tag with the given property and content attributes.
+    def render_meta(property, content)
+      "<meta property=\"#{property}\" content=\"#{content}\" />"
+    end
+  end
+end
+
+Liquid::Template.register_tag("render_og_image", Jekyll::RenderOGImageTag)
