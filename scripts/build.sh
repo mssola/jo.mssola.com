4908bfae28fcd8deb2fec6e694c434eefc9710cd
Miquel Sabaté Solà <mikisabate@gmail.com>
2024-09-17 07:21:28 +0200

    ci: Fix build
    Signed-off-by: Miquel Sabaté Solà <mikisabate@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/bash
+
+set -ex
 
 ROOT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 
-rm -r $ROOT_DIR/build/*
+rm -rf $ROOT_DIR/build
+mkdir $ROOT_DIR/build
 cp -r $ROOT_DIR/src/public/* $ROOT_DIR/build/
 find build/ -type f -name *.html -exec ruby scripts/render.rb {} \;
 
